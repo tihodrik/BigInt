297252ec9b29535dfa020d934cfd0262d7dd08d0
Darya Tikhomirova <tihodrik@gmail.com>
2017-04-03 15:36:39 +0300

    not finished yet

---
+++ polinom/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace polinom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.K = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.B = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Base = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(12, 328);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(315, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Рассчитать";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // K
+            // 
+            this.K.Location = new System.Drawing.Point(12, 50);
+            this.K.Multiline = true;
+            this.K.Name = "K";
+            this.K.Size = new System.Drawing.Size(171, 272);
+            this.K.TabIndex = 2;
+            this.K.Text = "1\r\n2\r\n3\r\n4\r\n5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите коэффициенты";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 412);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(315, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Результат";
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(201, 50);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(115, 20);
+            this.B.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите точку";
+            // 
+            // Base
+            // 
+            this.Base.Location = new System.Drawing.Point(201, 119);
+            this.Base.Name = "Base";
+            this.Base.Size = new System.Drawing.Size(114, 20);
+            this.Base.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(198, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Введите кольцо";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 453);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Base);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.K);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox K;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox B;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Base;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ polinom/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polinom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+        private List<int> result()
+        {
+            List<List<int>> a = GetNum(GetText(Owner.Controls["K"] as TextBox));
+            List<int> b = GetNum(GetText(Owner.Controls["B"] as TextBox)).First();
+            List<List<int>> p = new List<List<int>>();
+
+            int n = a.Count;
+
+            p.Add(a[0]);
+
+            for (int i = 1; i < n; i++)
+            {
+                p[i] = Sum(karatsuba_mul(p[i - 1], b), a[n - i]);
+            }
+            return p.Last();
+        }
+
+        private List<int> Sum(List<int> A, List<int> B)
+        {
+            int count = A.Count;
+            if (B.Count > count)
+            {
+                count = B.Count;
+                MakeEqual(ref A, count);
+            }
+            MakeEqual(ref B, count);
+
+            List<int> res = new List<int>(count);
+            bool f = false;
+
+            for (int i = 0; i < count; i++)
+            {
+
+                res[i] = (A[i] + B[i]) % 10;
+                if (f)
+                {
+                    res[i]++;
+                }
+
+                f = Convert.ToBoolean((A[i] + B[i]) / 10);
+            }
+
+            if (f)
+            {
+                res.Insert(0, 1);
+            }
+            return res;
+        }
+
+        private void MakeEqual(ref List<int> num, int count)
+        {
+            for (int i = 0; i < count - num.Count; i++)
+            {
+                num.Insert(0, 0);
+            }
+        }
+        private List<int> naive_mul(List<int> A, List<int> B) {
+            string tmp = A.ToString();
+            int A1 = Int32.Parse(tmp);
+
+            tmp = B.ToString();
+            int B1 = Int32.Parse(tmp);
+
+            int resINT = A1 * B1;
+            
+            string[] resSTR = new string[1];
+            resSTR[0].Insert(0, resINT.ToString());
+            return GetNum(resSTR).First();
+        }
+        private List<int> karatsuba_mul(List<int> A, List<int> B) {
+
+            int max = 9;
+
+            if (A.Count + B.Count < max)
+            {
+                return naive_mul(A, B);
+            }
+
+            int count = A.Count;
+            if (B.Count > count)
+            {
+                count = B.Count;
+            }
+            if (count % 2 != 0)
+            {
+                count++;
+            }
+            MakeEqual(ref A, count);
+            MakeEqual(ref B, count);
+
+            List<int> A1 = new List<int>(A.GetRange(0,count/2));
+            List<int> A2 = new List<int>(A.GetRange(count/2, count));
+            List<int> B1 = new List<int>(B.GetRange(0, count / 2));
+            List<int> B2 = new List<int>(B.GetRange(count / 2, count));
+
+            List<int> MUL1 = karatsuba_mul(A1, B1);
+            List<int> MUL2 = karatsuba_mul(A2, B2);
+            List<int> MUL3 = karatsuba_mul(Sum(A1,A2), Sum(B1,B2));
+
+            RemoveNulls(ref MUL1);
+            RemoveNulls(ref MUL2);
+            RemoveNulls(ref MUL3);
+
+            List<int> res = new List<int>();
+            res.AddRange(MUL1);
+
+
+        }
+
+        private void RemoveNulls(ref List<int> item)
+        {
+            int i = 0;
+            while (item[i] == 0)
+            {
+                i++;
+            }
+            item.RemoveRange(0, i);
+        }
+        private string[] GetText(TextBox item)
+        {
+            return item.Lines;
+        }
+        private List<List<int>> GetNum(string[] item)
+        {
+            List<List<int>> number = new List<List<int>>();
+            List<int> tmp = new List<int>();
+
+            for (int i = 0; i < item.Count(); i++)
+            {
+                for (int j = 0; j < item[i].Length; j++)
+                    tmp.Add((int)(item[i][j] - 48));
+
+                number.Add(tmp);
+                tmp.Clear();
+            }
+            return number;
+        }
+    }
+}
+
