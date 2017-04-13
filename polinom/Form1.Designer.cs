76d09fc5ef3e9e5044f58522a691974f4ae3ea7d
Darya Tikhomirova <tihodrik@gmail.com>
2017-04-13 21:27:23 +0300

    Mod by difference

--- polinom/Form1.Designer.cs
+++ polinom/Form1.Designer.cs
@@ -31,11 +31,11 @@
             this.button = new System.Windows.Forms.Button();
             this.K = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.R = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.B = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.Base = new System.Windows.Forms.TextBox();
+            this.Mod = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -56,7 +56,7 @@
             this.K.Name = "K";
             this.K.Size = new System.Drawing.Size(171, 272);
             this.K.TabIndex = 2;
-            this.K.Text = "1\r\n2\r\n3\r\n4\r\n5";
+            this.K.Text = "11024711\r\n20128731\r\n20200000";
             // 
             // label1
             // 
@@ -67,13 +67,13 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Введите коэффициенты";
             // 
-            // textBox1
+            // R
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 412);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(315, 20);
-            this.textBox1.TabIndex = 4;
+            this.R.Location = new System.Drawing.Point(12, 412);
+            this.R.Name = "R";
+            this.R.ReadOnly = true;
+            this.R.Size = new System.Drawing.Size(315, 20);
+            this.R.TabIndex = 4;
             // 
             // label2
             // 
@@ -90,6 +90,7 @@
             this.B.Name = "B";
             this.B.Size = new System.Drawing.Size(115, 20);
             this.B.TabIndex = 6;
+            this.B.Text = "100";
             // 
             // label3
             // 
@@ -100,12 +101,13 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Введите точку";
             // 
-            // Base
+            // Mod
             // 
-            this.Base.Location = new System.Drawing.Point(201, 119);
-            this.Base.Name = "Base";
-            this.Base.Size = new System.Drawing.Size(114, 20);
-            this.Base.TabIndex = 8;
+            this.Mod.Location = new System.Drawing.Point(201, 119);
+            this.Mod.Name = "Mod";
+            this.Mod.Size = new System.Drawing.Size(114, 20);
+            this.Mod.TabIndex = 8;
+            this.Mod.Text = "3";
             // 
             // label4
             // 
@@ -122,11 +124,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(355, 453);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.Base);
+            this.Controls.Add(this.Mod);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.B);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.R);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.K);
             this.Controls.Add(this.button);
@@ -142,11 +144,11 @@
         private System.Windows.Forms.Button button;
         private System.Windows.Forms.TextBox K;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox R;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox B;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox Base;
+        private System.Windows.Forms.TextBox Mod;
         private System.Windows.Forms.Label label4;
     }
 }
--- polinom/Form1.cs
+++ polinom/Form1.cs
@@ -12,6 +12,8 @@ namespace polinom
 {
     public partial class Form1 : Form
     {
+        List<int> m;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,80 +21,172 @@ namespace polinom
 
         private void button_Click(object sender, EventArgs e)
         {
-
-
-
+            R.Clear();
+            List<int> result = Result();
+            result.Reverse();
+            foreach (var digit in result)
+                R.AppendText(digit.ToString());
         }
-        private List<int> result()
+        
+        // Вычисление значения многочлена в точке
+        private List<int> Result()
         {
-            List<List<int>> a = GetNum(GetText(Owner.Controls["K"] as TextBox));
-            List<int> b = GetNum(GetText(Owner.Controls["B"] as TextBox)).First();
+            m = new List<int>();
+            string str = GetText(this.Controls["Mod"] as TextBox).First();
+            for (int i = 0; i < str.Length; i++)
+            {
+                m.Add((int)(str[i] - 48));
+            }
+            m.Reverse();
+
+            List<List<int>>a = GetList(GetText(this.Controls["K"] as TextBox));
+            List<int> b = GetList(GetText(this.Controls["B"] as TextBox)).First();
             List<List<int>> p = new List<List<int>>();
 
             int n = a.Count;
 
+            // Ввод коэффициентов должен начинаться со старшей степени: an,..., a0
+            // Все исходные значения уже взяты по модулю
             p.Add(a[0]);
 
             for (int i = 1; i < n; i++)
             {
-                p[i] = Sum(karatsuba_mul(p[i - 1], b), a[n - i]);
+                p.Add(Module(Sum(karatsuba_mul(p[i - 1], b), a[i])));
             }
-            return p.Last();
+                      
+            List<int> result = p.Last();
+            RemoveNulls(ref result);
+
+            return result;
         }
 
+        // Сумма больших чисел
         private List<int> Sum(List<int> A, List<int> B)
         {
             int count = A.Count;
             if (B.Count > count)
             {
                 count = B.Count;
-                MakeEqual(ref A, count);
+                AddNulls(ref A, count);
             }
-            MakeEqual(ref B, count);
+            else
+            {
+                if (B.Count < count)
+                {
+                    AddNulls(ref B, count);
+                }
+            }
 
-            List<int> res = new List<int>(count);
+            List<int> result = new List<int>(count);
             bool f = false;
 
             for (int i = 0; i < count; i++)
             {
-
-                res[i] = (A[i] + B[i]) % 10;
                 if (f)
                 {
-                    res[i]++;
+                    result.Add((A[i] + B[i] + 1) % 10);
+                    f = false;
+                   
+                    if ((A[i] + B[i] + 1) / 10 > 0)
+                    {
+                        f = true;
+                    }
                 }
-
-                f = Convert.ToBoolean((A[i] + B[i]) / 10);
+                else
+                {
+                    result.Add((A[i] + B[i]) % 10);
+                    if ((A[i] + B[i]) / 10 > 0)
+                    {
+                        f = true;
+                    }
+                }
             }
-
             if (f)
             {
-                res.Insert(0, 1);
+                result.Add(1);
             }
-            return res;
-        }
 
-        private void MakeEqual(ref List<int> num, int count)
+            return result;
+        }
+        // Разность больших чисел
+        private List<int> Dif(List<int> A, List<int> B)
         {
-            for (int i = 0; i < count - num.Count; i++)
+            // A > B
+            int count = A.Count;
+            if (B.Count > count)
             {
-                num.Insert(0, 0);
+                count = B.Count;
+                AddNulls(ref A, count);
+            }
+            else
+            {
+                if (B.Count < count)
+                {
+                    AddNulls(ref B, count);
+                }
+            }
+
+            List<int> result = new List<int>(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                if (A[i] < B[i])
+                {
+                    A[i + 1]--;
+                    result.Add(A[i] + 10 - B[i]);
+                }
+                else
+                {
+                    result.Add(A[i] - B[i]);
+                }
+            }
+            return result;
+        }
+        // Добавление незначащих нулей
+        private void AddNulls(ref List<int> item, int count)
+        {
+            // Все листы реверсированы, значит, самый старший разряд
+            // имеет самый большой номер в массиве
+            int c = item.Count;
+            while (c < count)
+            {
+                item.Add(0);
+                c++;
             }
         }
-        private List<int> naive_mul(List<int> A, List<int> B) {
-            string tmp = A.ToString();
-            int A1 = Int32.Parse(tmp);
+        // Удаление незначащих нулей
+        private void RemoveNulls(ref List<int> item)
+        {
+            // Все листы реверсированы, значит, самый старший разряд
+            // имеет самый большой номер в массиве
+            int i = 0;
+            int count = item.Count;
+            while ( (count - i - 1) > 0 && item[count - i - 1] == 0)
+            {
+                i++;
+            }
+            item.RemoveRange(count - i, i);
+        }
 
-            tmp = B.ToString();
-            int B1 = Int32.Parse(tmp);
+        // Обычное умножение
+        private List<int> naive_mul(List<int> A, List<int> B)
+        {
+            int A1 = GetNum(A);
+            int B1 = GetNum(B);
 
             int resINT = A1 * B1;
-            
+
             string[] resSTR = new string[1];
-            resSTR[0].Insert(0, resINT.ToString());
-            return GetNum(resSTR).First();
+            resSTR[0] = resINT.ToString();
+            // Приведение по модулю выполнится за счет GetList
+            return GetList(resSTR).First();
         }
-        private List<int> karatsuba_mul(List<int> A, List<int> B) {
+
+        // Умножение методом Карацубы
+        private List<int> karatsuba_mul(List<int> A, List<int> B)
+        {
+            RemoveNulls(ref A);
+            RemoveNulls(ref B);
 
             int max = 9;
 
@@ -110,55 +204,188 @@ namespace polinom
             {
                 count++;
             }
-            MakeEqual(ref A, count);
-            MakeEqual(ref B, count);
+            AddNulls(ref A, count);
+            AddNulls(ref B, count);
 
-            List<int> A1 = new List<int>(A.GetRange(0,count/2));
-            List<int> A2 = new List<int>(A.GetRange(count/2, count));
+            List<int> A1 = new List<int>(A.GetRange(0, count / 2));
+            List<int> A2 = new List<int>(A.GetRange(count / 2, count / 2));
             List<int> B1 = new List<int>(B.GetRange(0, count / 2));
-            List<int> B2 = new List<int>(B.GetRange(count / 2, count));
+            List<int> B2 = new List<int>(B.GetRange(count / 2, count / 2));
 
+            List<int> sumA = Sum(A1, A2);
+            List<int> sumB = Sum(B1, B2);
+
+            RemoveNulls(ref A1);
+            RemoveNulls(ref A2);
+            RemoveNulls(ref B1);
+            RemoveNulls(ref B2);
+            
+            RemoveNulls(ref sumA);
+            RemoveNulls(ref sumB);
+            
             List<int> MUL1 = karatsuba_mul(A1, B1);
             List<int> MUL2 = karatsuba_mul(A2, B2);
-            List<int> MUL3 = karatsuba_mul(Sum(A1,A2), Sum(B1,B2));
-
+            List<int> MUL3 = karatsuba_mul(sumA, sumB);
+            
+            MUL3 = Dif(MUL3, Sum(MUL1, MUL2));
+            
             RemoveNulls(ref MUL1);
             RemoveNulls(ref MUL2);
             RemoveNulls(ref MUL3);
 
-            List<int> res = new List<int>();
-            res.AddRange(MUL1);
-
-
-        }
-
-        private void RemoveNulls(ref List<int> item)
-        {
-            int i = 0;
-            while (item[i] == 0)
+            List<int> result = new List<int>();
+            result.Add(0);
+            if (!(MUL2.Count == 1 && MUL2.First() == 0))
             {
-                i++;
+                for (int i = 0; i < count; i++)
+                {
+                    MUL2.Insert(0, 0);
+                }
             }
-            item.RemoveRange(0, i);
+            if (!(MUL3.Count == 1 && MUL3.First() == 0))
+            {
+                for (int i = 0; i < count / 2; i++)
+                {
+                    MUL3.Insert(0, 0);
+                }
+            }
+
+            result = Sum(result, MUL2);
+            result = Sum(result, MUL3);
+            result = Sum(result, MUL1);
+            
+            RemoveNulls(ref result);
+                        
+            return Module(result);
         }
+
+        private List<int> Module(List<int> item)
+        {
+            RemoveNulls(ref item);
+
+            bool minus = false;
+            
+            if (item[item.Count - 1] < 0)
+            {
+                minus = true;
+                item[item.Count - 1] *= (-1);
+            }
+
+            // Приведение отрицательных чисел
+            if (minus)
+            {
+                if (Compare(item, m) <= 0)
+                {
+                    return Dif(m, item);
+                }
+                else
+                {
+                    while (Compare(item, m) > 0)
+                    {
+                        item = Dif(item, m);
+                    }
+                    item = Dif(m, item);
+                }
+            }
+            // Приведние положительных чисед
+            else
+            {
+                while (Compare(item, m) >= 0)
+                {
+                    item = Dif(item, m);
+                }
+            }
+            return item;
+        }
+
+        private int Compare(List<int> A, List<int> B)
+        {
+            int count = A.Count;
+            if (B.Count > count)
+            {
+                count = B.Count;
+            }
+            AddNulls(ref A, count);
+            AddNulls(ref B, count);
+
+            for (int i = count-1; i >= 0; i--)
+            {
+                if (A[i] < B[i])
+                    return -1;
+                if (A[i] > B[i])
+                    return 1;                 
+            }
+            return 0;            
+        }
+
+
         private string[] GetText(TextBox item)
         {
             return item.Lines;
         }
-        private List<List<int>> GetNum(string[] item)
+        private List<List<int>> GetList(string[] item)
         {
-            List<List<int>> number = new List<List<int>>();
-            List<int> tmp = new List<int>();
+            List<List<int>> list = new List<List<int>>();
 
             for (int i = 0; i < item.Count(); i++)
             {
-                for (int j = 0; j < item[i].Length; j++)
-                    tmp.Add((int)(item[i][j] - 48));
+                if (item[i] == "")
+                {
+                    continue;
+                }
+                List<int> tmp = new List<int>();
+                int j = 0;
 
-                number.Add(tmp);
-                tmp.Clear();
+                if (item[i][j] == '-') {
+                    j++;
+                    tmp.Add((int)(item[i][j] - 48)*(-1));
+                    j++;
+                }
+
+                for ( ; j < item[i].Length; j++)
+                {
+                    tmp.Add((int)(item[i][j] - 48));
+                }
+
+                tmp.Reverse();
+                list.Add(Module(tmp));
             }
-            return number;
+            return list;
+        }
+
+        // Перевод вектора в число
+        private int GetNum(List<int> A)
+        {
+            int result = 0;
+            bool minus = false;
+
+            RemoveNulls(ref A);
+
+            if (A[A.Count - 1] < 0)
+            {
+                minus = true;
+                A[A.Count - 1] *= -1;
+            }
+
+            for (int i=0; i < A.Count; i++)
+            {
+                result += A[i] * Power(10, i);
+            }
+
+            if (minus)
+                return result * (-1);
+            return result;
+        }
+
+        // Возведение числа p в степень k
+        private int Power(int p, int k)
+        {
+            int result = 1;
+            for (int i = 0; i < k; i++)
+            {
+                result *= p;
+            }
+            return result;
         }
     }
 }
